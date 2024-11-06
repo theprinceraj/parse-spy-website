a51ba01b4c20dbed7704e5fa419ba004a00dea5a
theprinceraj <profile.princeraj@gmail.com>
2024-11-06 21:42:11 +0530

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { Navbar } from "./components/Navbar/Navbar.tsx";
+
+function App() {
+    return (
+        <>
+            <Navbar />
+        </>
+    );
+}
+
+export default App;
---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,14 @@
+import "./Navbar.css";
+export const Navbar = () => {
+    return (
+        <div>
+            <div>
+                <div>{/*    Icon Placeholder    */}</div>
+                <div>
+                    <text>ParseSpy</text>
+                </div>
+            </div>
+            <div></div>
+        </div>
+    );
+};
