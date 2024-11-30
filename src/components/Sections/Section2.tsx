6bef47e75a7029d972e93a8946de9dd5d8a8a2f7
theprinceraj <profile.princeraj@gmail.com>
2024-11-30 08:01:06 +0530

    "Renamed CSS class from 'feature-cards' to 'feature-grid' and updated corresponding HTML structure in Section2 component."

--- src/components/Sections/Section2.tsx
+++ src/components/Sections/Section2.tsx
@@ -1,16 +1,56 @@
 import "./Section2.css";
 import { Section } from "../SectionTemplate/Section";
-
+import CheckIconSVG from "../../assets/check-icon.svg";
 export const Section2 = () => {
     return (
         <Section
             floatingBoxText={null}
             heading={["Secure investigations.", "Anytime, anywhere."]}
             description={["Begin in under 5 minutes.", "Start capturing insights from anywhere, anytime."]}>
-            <div className="feature-cards">
-                <div></div>
-                <div></div>
-                <div></div>
+            <div className="features-grid">
+                <div className="features-card" id="features-card-1">
+                    <div className="feature-card-visual">
+                        <div>
+                            <p>Monthly fee</p>
+                            <h5>$69</h5>
+                            <h6>
+                                <span>
+                                    <img src={CheckIconSVG} className="check-icon" alt="Check Icon" />
+                                </span>
+                                <span>All inclusive</span>
+                            </h6>
+                        </div>
+                    </div>
+                    <div className="feature-card-content">
+                        <h6 className="feature-card-heading">Simple</h6>
+                        <p className="feature-card-description">
+                            A predictable phone plan
+                            <br />
+                            without costly roaming fees.
+                        </p>
+                    </div>
+                </div>
+                <div className="features-card" id="features-card-2">
+                    <div className="feature-card-visual"></div>
+                    <div className="feature-card-content">
+                        <h6 className="feature-card-heading">Fast & Easy</h6>
+                        <p className="feature-card-description">
+                            Get started in minutes. Just scan a<br />
+                            QR code and you’re ready to go.
+                        </p>
+                    </div>
+                </div>
+                <div className="features-card" id="features-card-3">
+                    <div className="feature-card-visual"></div>
+                    <div className="feature-card-content">
+                        <h6 className="feature-card-heading">Expat Friendly</h6>
+                        <p className="feature-card-description">
+                            No SSN or US bank needed.
+                            <br />
+                            Activate before arriving in the US.
+                        </p>
+                    </div>
+                </div>
             </div>
         </Section>
     );
